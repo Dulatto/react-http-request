027a989f0a0615633891621dfe2bca7a3b14a369
Dulatto <dulat.omarov@gmail.com>
2021-06-07 22:15:09 -0400

    link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import NewPost from './NewPost/NewPost';
@@ -15,8 +15,10 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
