980f861dc57c0e74d90734e5af37aac626747b34
Dulatto <dulat.omarov@gmail.com>
2021-06-12 23:43:35 -0400

    redirect

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
 import { Route } from 'react-router-dom';
-
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 import FullPost from '../FullPost/FullPost';
 
 export default class Posts extends Component {
-
     state = {
         posts: []
     }
